d611831f68fed718151bea5cc72fe1cda8fd33a2
Joshua Fong <jwfong89@gmail.com>
2016-09-18 17:21:00 -0700

    deployment

---
+++ client/app.js
@@ -0,0 +1,66 @@
+angular.module('app', [])
+
+.controller('MainController', function($scope, Chuck){
+  // angular.extend($scope);
+	$scope.getChuck = function() {
+		Chuck.getData()
+		  .then(function(data){
+		  	$scope.chuck = data.value;
+		  })
+		  .catch(function(err){
+		  	console.error(err);
+		  });
+	};
+})
+
+// .factory('Todos', function() {
+// 	var todos = [];
+	
+// 	var addTodo = function(title){
+// 		todos.push({title: title, due: Date.now() });
+// 	};
+
+// 	var done = function(index) {
+// 		todos.splice(index,1);
+// 	};
+
+// 	return {
+// 		todos: todos,
+// 		addTodo: addTodo,
+// 		done: done
+// 	};
+// })
+
+
+
+.factory('Chuck', function($http){
+	var jokes = [];
+    var getData = function() {
+		return $http({
+			method: 'get',
+			url: 'https://api.chucknorris.io/jokes/random'
+		})
+		.then(function(resp){
+			jokes.push(resp.data.value);
+			return resp.data;
+		})
+	};
+	var getJoke = function(){
+		jokes.push(getData.value);	
+	};
+
+
+	return {
+		jokes: jokes,
+		getData: getData,
+		getJoke: getJoke
+	};
+})
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var http = require('http');
+
+
+var app = express();
+app.use(express.static('client'));
+
+// app.get('/', function(request, response) {
+//   response.sendFile(__dirname + '/client/index.html')
+//   response.write('hello world');
+//   response.end();
+// })
+
+
+
+console.log('The Server is Working on port 8000!!!!')
+app.listen(8000);
