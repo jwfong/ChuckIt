9153142cc5233e9a46cb1205e8262dd544d3c4d7
Joshua Fong <jwfong89@gmail.com>
2016-09-18 19:09:23 -0700

    removed import statement

--- client/app.js
+++ client/app.js
@@ -1,41 +1,39 @@
-import angular from 'angular';
-
 angular.module('app', [])
-.controller('MainController', function($scope, Chuck){
-  // angular.extend($scope);
-	$scope.getChuck = function() {
-		Chuck.getData()
-		  .then(function(data){
-		  	$scope.chuck = data.value;
-		  })
-		  .catch(function(err){
-		  	console.error(err);
-		  });
-	};
-})
-.factory('Chuck', function($http){
-	var jokes = [];
-    var getData = function() {
-		return $http({
-			method: 'get',
-			url: 'https://api.chucknorris.io/jokes/random'
-		})
-		.then(function(resp){
-			jokes.push(resp.data.value);
-			return resp.data;
-		})
-	};
-	var getJoke = function(){
-		jokes.push(getData.value);	
-	};
+	.controller('MainController', function($scope, Chuck){
+	  // angular.extend($scope);
+		$scope.getChuck = function() {
+			Chuck.getData()
+			  .then(function(data){
+			  	$scope.chuck = data.value;
+			  })
+			  .catch(function(err){
+			  	console.error(err);
+			  });
+		};
+	})
+	.factory('Chuck', function($http){
+		var jokes = [];
+	    var getData = function() {
+			return $http({
+				method: 'get',
+				url: 'https://api.chucknorris.io/jokes/random'
+			})
+			.then(function(resp){
+				jokes.push(resp.data.value);
+				return resp.data;
+			})
+		};
+		var getJoke = function(){
+			jokes.push(getData.value);	
+		};
 
 
-	return {
-		jokes: jokes,
-		getData: getData,
-		getJoke: getJoke
-	};
-})
+		return {
+			jokes: jokes,
+			getData: getData,
+			getJoke: getJoke
+		};
+	})
 
 
 
